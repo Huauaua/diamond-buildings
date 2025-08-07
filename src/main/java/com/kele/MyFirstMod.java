0fd4584accb995bf76e2e996763a9da997b42fe4
Huauaua <13658736+hauauah@user.noreply.gitee.com>
2025-08-07 17:17:00 +0800

    add item assets

--- src/main/java/com/kele/MyFirstMod.java
+++ src/main/java/com/kele/MyFirstMod.java
@@ -1,5 +1,6 @@
 package com.kele;
 
+import com.kele.moditems.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class MyFirstMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		ModItems.registerItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
